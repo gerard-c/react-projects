ae3a823750a9857fc929396ca20d9cd2698e0695
Gerard Compion <gcompion93@gmail.com>
2022-04-06 16:53:52 -0400

    setup menu component to render unique menu items using dataas props

--- 05-menu/setup/src/Menu.js
+++ 05-menu/setup/src/Menu.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({ items }) => {
+
+  const menuItems = items.map((item) => {
+    const { id, title, img, desc, price } = item;
+    return (
+      <article key={id} className="menu-item">
+        <img src={img} alt={title} className="photo" />
+        <div className="item-info">
+          <header>
+            <h4>
+              {title}
+            </h4>
+            <h4 className="price">${price}</h4>
+          </header>
+          <p className="item-text">{desc}</p>
+        </div>
+      </article>
+    );
+  })
+
+  return (
+    <div className="section-center">
+      {menuItems}
+    </div>
+  )
 };
 
 export default Menu;
