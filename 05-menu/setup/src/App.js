d01e8f33b08ba547b3acb86e2d049fc260fbca1b
Gerard Compion <gcompion93@gmail.com>
2022-04-06 17:15:04 -0400

    setup basic, manually added category buttons

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -6,6 +6,16 @@ import items from './data';
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  }
+
   return (
     <main>
       <section className="menu section">
@@ -13,7 +23,7 @@ function App() {
           <h2>Our Menu</h2>
           <div className="underline" />
         </div>
-        <Categories />
+        <Categories filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ filterItems }) => {
+
+  const buttons = 'oi-oi-oi-oi';
+
+  return (
+    <div className="btn-container">
+      <button className="filter-btn" onClick={() => filterItems('all')}>All</button>
+      <button className="filter-btn" onClick={() => filterItems('breakfast')}>Breakfast</button>
+    </div>
+  );
 };
 
 export default Categories;
