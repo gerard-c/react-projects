4d9f53577394d9c6802f00797dbc2d4ca0e98b03
Gerard Compion <gcompion93@gmail.com>
2022-04-08 13:42:57 -0400

    move title to its own component

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
+import Title from './components/Title';
 import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
@@ -27,11 +28,7 @@ function App() {
 
   return (
     <section className="section">
-      <div className="title">
-        <h2>
-          <span>/</span>reviews
-        </h2>
-      </div>
+      <Title />
       <div className="section-center">
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
