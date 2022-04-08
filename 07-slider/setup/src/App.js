3065da1943675bfd4c6de808b3cec9ac7daafc3a
Gerard Compion <gcompion93@gmail.com>
2022-04-08 12:40:36 -0400

    setup basic state for people, index

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -3,7 +3,10 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <h2>slider project setup</h2>
+  );
 }
 
 export default App;
