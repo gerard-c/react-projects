d097698af736fbfca5884ed66f2d59519e6bcf7e
Gerard Compion <gcompion93@gmail.com>
2022-04-13 16:42:15 -0400

    add basic logic to display single color components with relevant info from API

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -37,7 +37,9 @@ function App() {
         </form>
       </section>
       <section className="colors">
-        <h4>List Goes Here</h4>
+        {list.map((color, index) => {
+          return <SingleColor key={index} {...color} index={index} />
+        })}
       </section>
     </>
   )
