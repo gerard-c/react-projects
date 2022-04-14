a6f5b7c180fbfb7f5495572729e34740a56c5b43
Gerard Compion <gcompion93@gmail.com>
2022-04-14 17:07:01 -0400

    remove seperate components for social and sidebar for simplicity's sake, begin setup of useRef usage

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,11 +1,17 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { FaBars, FaTwitter } from 'react-icons/fa';
-import Sidebar from './Sidebar';
-import Social from './Social';
+import { links, social } from './data';
 import logo from './logo.svg';
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  console.log(linksRef);
+
+  // useEffect(() => {
+  //   const linksHeight = linksRef.current.getBoundingClientRect();
+  // }, [showLinks])
 
   return (
     <nav>
@@ -16,8 +22,28 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        <Sidebar showLinks={showLinks}/>
-        <Social />
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </nav>
   );
